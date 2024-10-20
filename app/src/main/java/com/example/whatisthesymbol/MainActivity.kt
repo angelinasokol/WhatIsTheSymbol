7e70db01e376e686dc829f6df9e527696edd9b5d
sokko <angelinasokol2005@gmail.com>
2024-10-20 21:48:12 +0700

    Пишем проверку

--- app/src/main/java/com/example/whatisthesymbol/MainActivity.kt
+++ app/src/main/java/com/example/whatisthesymbol/MainActivity.kt
@@ -4,24 +4,32 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val handing : TextView = findViewById(R. id.handing)
-        val button : Button = findViewById(R. id.button)
-        val inputSymbol : EditText = findViewById(R. id.inputSymbol)
-        button.setOnClickListener{
-            val charInput = inputSymbol.text.toString()
-            if (charInput.length == 1) {
-                val char = charInput[0]
-            }
+
+        val handing: TextView = findViewById(R.id.handing)
+        val button: Button = findViewById(R.id.button)
+        val inputSymbol: EditText = findViewById(R.id.inputSymbol)
+
+        button.setOnClickListener {
+            val symbolInput = inputSymbol.text.toString()
+            if (symbolInput.length == 1) {
+                val symbol = symbolInput[0]
+
+                if (symbol.isDigit()) {
+                    handing.text = "Это цифра!"
+                } else if (symbol in setOf('&', '#', '<', '–')) {
+                    handing.text = "Это специальный символ!"
+                } else {
+                    handing.text = "Непредусмотренный вариант!"
+                }
+            } else {
+                handing.text = "Ошибка: введите один символ"
             }
         }
-            }
-        }
+    }
+}

5583cb565544df1b828933430b6a0607c3915003
sokko <angelinasokol2005@gmail.com>
2024-10-20 22:28:33 +0700

    Добавим задержку перед проверкой символа

--- app/src/main/java/com/example/whatisthesymbol/MainActivity.kt
+++ app/src/main/java/com/example/whatisthesymbol/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.whatisthesymbol
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -14,19 +16,30 @@ class MainActivity : AppCompatActivity() {
         val handing: TextView = findViewById(R.id.handing)
         val button: Button = findViewById(R.id.button)
         val inputSymbol: EditText = findViewById(R.id.inputSymbol)
+        val handler = Handler(Looper.getMainLooper())
 
         button.setOnClickListener {
             val symbolInput = inputSymbol.text.toString()
             if (symbolInput.length == 1) {
                 val symbol = symbolInput[0]
 
-                if (symbol.isDigit()) {
-                    handing.text = "Это цифра!"
-                } else if (symbol in setOf('&', '#', '<', '–')) {
-                    handing.text = "Это специальный символ!"
-                } else {
-                    handing.text = "Непредусмотренный вариант!"
-                }
+                handing.text = "Проверяем, является ли символ цифрой..."
+                handler.postDelayed({
+                    if (symbol.isDigit()) {
+                        handing.text = "Это цифра!"
+                    } else {
+                        handing.text = "Символ не является цифрой. Проверяем, является ли он специальным символом..."
+
+
+                        handler.postDelayed({
+                            if (symbol in setOf('&', '#', '<', '–')) {
+                                handing.text = "Это специальный символ!"
+                            } else {
+                                handing.text = "Символ не является специальным. Непредусмотренный вариант!"
+                            }
+                        }, 1000) // 1000 миллисекунд = 1 секунда
+                    }
+                }, 1000) // 1000 миллисекунд = 1 секунда
             } else {
                 handing.text = "Ошибка: введите один символ"
             }

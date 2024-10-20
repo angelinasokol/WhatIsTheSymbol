c93314ae59a8358c868c9ae1a6fe365ef1ac2908
sokko <angelinasokol2005@gmail.com>
2024-10-20 16:27:30 +0700

    Найдем элементы по ID

--- app/src/main/java/com/example/whatisthesymbol/MainActivity.kt
+++ app/src/main/java/com/example/whatisthesymbol/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.whatisthesymbol
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+            val handing : TextView = findViewById(R. id.handing)
+            val button : Button = findViewById(R. id.button)
+            val inputSymbol : EditText = findViewById(R. id.inputSymbol)
 
+            }
+        }
     }
 }
